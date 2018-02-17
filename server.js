e13caf04f1be0d9e3e9c8e4716ee99ce254ca84f
james sewell <jamesjosephsewell@gmail.com>
2018-02-16 22:52:40 -0600

    added a serverjs file for local testing on port 3000, use node index.html in the command line to serve it up

---
+++ server.js
@@ -0,0 +1,8 @@
+const express = require('express')
+const app = express()
+const game = './index.html'
+
+app.get('/', (req, res) => res.sendfile(game))
+app.use(express.static('public'))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
